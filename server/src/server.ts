d3a15a8eb5f6cd58bc436ff46beffdb82dedf305
Hyago Silva <hyeghyago@gmail.com>
2023-01-18 03:04:48 +0000

    criando api

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors';
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors)
+
+app.get('/habits', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Beber'
+            }
+        }
+    })
+
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP server is running on port 3333')
+});
